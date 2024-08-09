b6647937610c48371941e34a542633e6508d5fe5
GYUHWAN JEONG <105405834+solp721@users.noreply.github.com>
2024-08-10 02:28:29 +0900

    fix : api key값 명시로 변경

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
 				<Main />
 				<NextScript />
 				<Script
-					src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_JS_KEY}&libraries=services,clusterer&autoload=false`}
+					src="//dapi.kakao.com/v2/maps/sdk.js?appkey=af3baec1e41a89a148ac199d93d21e37&libraries=services,clusterer&autoload=false"
 					strategy="beforeInteractive"
 				/>
 			</body>
