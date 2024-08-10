a1695a2b998143bb3dbd46c28242682912dd77b8
GYUHWAN JEONG <105405834+solp721@users.noreply.github.com>
2024-08-10 18:58:46 +0900

    feat : 개발용,배포용 api key 추가

--- pages/_document.js
+++ pages/_document.js
@@ -16,3 +16,11 @@ export default function Document() {
 		</Html>
 	);
 }
+
+// 개발용
+// 376c6dbd40a9160d6cb060f21074e427
+// src="//dapi.kakao.com/v2/maps/sdk.js?appkey=376c6dbd40a9160d6cb060f21074e427&libraries=services,clusterer&autoload=false"
+
+// 배포용
+// af3baec1e41a89a148ac199d93d21e37
+// src="//dapi.kakao.com/v2/maps/sdk.js?appkey=af3baec1e41a89a148ac199d93d21e37&libraries=services,clusterer&autoload=false"
