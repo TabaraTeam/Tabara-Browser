f2a79c5f4a96f3e535fdafe936fd5d3900011af5
GYUHWAN JEONG <105405834+solp721@users.noreply.github.com>
2024-08-10 18:58:59 +0900

    feat : 현재 위치 테스트

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,65 @@
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+	const [state, setState] = useState({
+		center: {
+			lat: 33.450701,
+			lng: 126.570667,
+		},
+		errMsg: null,
+		isLoading: true,
+	});
+
+	useEffect(() => {
+		if (navigator.geolocation) {
+			// GeoLocation을 이용해서 접속 위치를 얻어옵니다
+			navigator.geolocation.getCurrentPosition(
+				position => {
+					setState(prev => ({
+						...prev,
+						center: {
+							lat: position.coords.latitude, // 위도
+							lng: position.coords.longitude, // 경도
+						},
+						isLoading: false,
+					}));
+				},
+				err => {
+					setState(prev => ({
+						...prev,
+						errMsg: err.message,
+						isLoading: false,
+					}));
+				},
+			);
+		} else {
+			// HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+			setState(prev => ({
+				...prev,
+				errMsg: 'geolocation을 사용할수 없어요..',
+				isLoading: false,
+			}));
+		}
+	}, []);
+
 	return (
-		<Map
-			center={{ lat: 33.5563, lng: 126.79581 }}
-			style={{ width: '100%', height: '360px' }}
+		<Map // 지도를 표시할 Container
+			center={state.center}
+			style={{
+				// 지도의 크기
+				width: '100%',
+				height: '450px',
+			}}
+			level={3} // 지도의 확대 레벨
 		>
-			<MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-				<div style={{ color: '#000' }}>Hello World!</div>
-			</MapMarker>
+			{!state.isLoading && (
+				<MapMarker position={state.center}>
+					<div style={{ padding: '5px', color: '#000' }}>
+						{state.errMsg ? state.errMsg : '여기에 계신가요?!'}
+					</div>
+				</MapMarker>
+			)}
 		</Map>
 	);
 }
