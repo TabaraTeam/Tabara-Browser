f65df35757cd4721cb88b8c9a52cb5c443f7281b
GYUHWAN JEONG <105405834+solp721@users.noreply.github.com>
2024-08-10 20:58:19 +0900

    feat : 현재 위치 트래킹

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,62 @@
+import { useEffect, useMemo, useState } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
-import { useEffect, useState } from 'react';
+import { debounce } from 'lodash';
 
 export default function Home() {
-	const [state, setState] = useState({
-		center: {
-			lat: 33.450701,
-			lng: 126.570667,
-		},
-		errMsg: null,
-		isLoading: true,
+	const [center, setCenter] = useState({
+		lat: 33.450701,
+		lng: 126.570667,
 	});
 
+	const [position, setPosition] = useState({
+		lat: 33.450701,
+		lng: 126.570667,
+	});
+
+	const setCenterToMyPosition = () => {
+		setCenter(position);
+	};
+
+	const updateCenterWhenMapMoved = useMemo(
+		() =>
+			debounce(map => {
+				console.log(map.getCenter());
+				setCenter({
+					lat: map.getCenter().getLat(),
+					lng: map.getCenter().getLng(),
+				});
+			}, 500),
+		[],
+	);
+
 	useEffect(() => {
-		if (navigator.geolocation) {
-			// GeoLocation을 이용해서 접속 위치를 얻어옵니다
-			navigator.geolocation.getCurrentPosition(
-				position => {
-					setState(prev => ({
-						...prev,
-						center: {
-							lat: position.coords.latitude, // 위도
-							lng: position.coords.longitude, // 경도
-						},
-						isLoading: false,
-					}));
-				},
-				err => {
-					setState(prev => ({
-						...prev,
-						errMsg: err.message,
-						isLoading: false,
-					}));
-				},
-			);
-		} else {
-			// HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-			setState(prev => ({
-				...prev,
-				errMsg: 'geolocation을 사용할수 없어요..',
-				isLoading: false,
-			}));
-		}
+		navigator.geolocation.getCurrentPosition(pos => {
+			setCenter({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+		});
+
+		navigator.geolocation.watchPosition(pos => {
+			setPosition({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+		});
 	}, []);
 
 	return (
-		<Map // 지도를 표시할 Container
-			center={state.center}
-			style={{
-				// 지도의 크기
-				width: '100%',
-				height: '450px',
-			}}
-			level={3} // 지도의 확대 레벨
-		>
-			{!state.isLoading && (
-				<MapMarker position={state.center}>
-					<div style={{ padding: '5px', color: '#000' }}>
-						{state.errMsg ? state.errMsg : '여기에 계신가요?!'}
-					</div>
-				</MapMarker>
-			)}
-		</Map>
+		<>
+			<Map
+				id="map"
+				center={center}
+				style={{
+					width: '100%',
+					height: '450px',
+				}}
+				level={3}
+				onCenterChanged={updateCenterWhenMapMoved}
+			>
+				<MapMarker position={position} />
+			</Map>
+			<div>
+				<button>Refresh</button>
+				<button onClick={setCenterToMyPosition}>My Location</button>
+			</div>
+		</>
 	);
 }
